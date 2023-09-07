3336a172b34ffdb7936316e256fd015b04536561
Isailton89 <isailton2008@gmail.com>
2023-09-06 23:52:15 -0300

    Aprendendo a utilizar variaveis

--- Trilha-java-basico/java-anatomia-classes/src/MinhaClasse.java
+++ Trilha-java-basico/java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,19 @@
 public class MinhaClasse {
     public static void main (String [] args) {
-        System.out.print ("Olá mundo");
+        //tipos de variaveis
+        // String meuNome = "Isailton";
+        // int anoFabricacao = 2022;
+        // boolean verdadeira = true;
+
+        String primeiroNome = "Isailton";
+        String segundoNome = "Alves";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
